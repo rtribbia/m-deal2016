26c98c439e380fc8c5296c143aaf60d234cb8c94
Robert Tribbia <rtribbia@gmail.com>
2016-01-10 21:24:04 -0800

    Added basic room functionality.

--- resources/manager.js
+++ resources/manager.js
@@ -5,30 +5,79 @@ function Manager(io) {
 	this.io = io;
 }
 
+
+//Retrieve list of all rooms where joinable==true
 Manager.prototype.getJoinable = function() {
 	var result = [];
 	this.states.forEach(function(state) {
-		if (state.joinable)
-			result.push(state);
+		var resultItem = {};
+		if (state.joinable) {
+			resultItem.id = state.id;
+			resultItem.playerCount = state.players.length;
+			result.push(resultItem);
+		}
 	});
 	return result;
 }
 
+
+//Is room ID joinable? Returns true or false
 Manager.prototype.isJoinable = function(roomId) {
 	return this.states[roomId].joinable;
 }
 
+
+//Creates game and pushes it to end of gamestate array. New game 'id' == index in gamestate array.
 Manager.prototype.createGame = function() {
 	var game = new GameState(this.states.length);
 	this.states.push(game);
-
 	console.log('Creating game: ' + game.id);
+	this.updateLobbyRoomList();
 }
 
-Manager.prototype.sendRoomList = function(socket) {
-	console.log('Sending rooms to ' + socket.id)
+
+//Send joinable room list to roomname or socket
+Manager.prototype.sendRoomList = function(target) {
 	var joinable = this.getJoinable();
-	this.io.to(socket.id).emit('roomList', joinable);
+
+	if ((typeof target) === "string") { //If roomname and not socket object.
+		if (target === "everyone") { //Send to EVERYONE connected
+			this.io.emit('roomList', joinable);
+		} else { //Send to specifc room
+			this.io.to(target).emit('roomList', joinable);
+		}
+	} else {
+		this.io.to(target.socket.id).emit('roomList', joinable);
+	}
+}
+
+
+//Send everyone in the lobby an updated room list
+Manager.prototype.updateLobbyRoomList = function() {
+	
+	this.sendRoomList('everyone');
+	
+	//this.sendRoomList('lobby');
+}
+
+
+//Add a player to a gamestate (room) at roomID
+Manager.prototype.addPlayerToRoom = function(player,roomId) {
+	var currentRoom = this.states[roomId];
+	var hasPlayer = currentRoom.hasPlayer(player);
+	if (hasPlayer != null) { //Player is trying to join same room.
+		console.log(player.id + ' is already in room # ' + roomId);
+		return false;
+	} else {
+		if (player.gameStatus()) { //Remove player from current room
+			this.states[player.currentLoc].removePlayer(player);
+		}
+		player.loc(roomId);
+		player.gameStatus(true);
+		currentRoom.addPlayer(player);
+		this.updateLobbyRoomList();
+		return true;
+	}
 }
 
 
--- resources/player.js
+++ resources/player.js
@@ -1,41 +1,39 @@
-function Player(socket, manager) {
+function Player(socket, manager, io) {
+	this.socket = socket;
+	this.manager = manager;
+	this.io = io;
 
 	this.id = socket.id;
 	this.handle = '';
-	this.socket = socket;
+	this.socket.player = this;
 	this.inGame = false;
-	this.currentLoc = '';
-	this.manager = manager;
+	this.currentLoc = null;
+	
 
 }
 
+//Getter/Setter for players inGame status.
+Player.prototype.gameStatus = function(newStatus) {
+	if (newStatus) {
+		this.inGame = newStatus;
+	} else {
+		return this.inGame;
+	}
+}
 
-Player.prototype.loc = function(newLoc) { //Getter/Setter for player location
-	if (newLoc) {
-		if (this.currentLoc)
+//Getter/Setter for player location
+Player.prototype.loc = function(newLoc) { 
+	if (newLoc != undefined) {
+		if (this.currentLoc != null)
 			this.socket.leave(this.currentLoc);
+
+		console.log(this.id + ': changing location \'' + this.currentLoc + '\' -> \'' + newLoc + '\'');
 		this.currentLoc = newLoc;
 		this.socket.join(newLoc);
 	} else {
 		return this.currentLoc;
 	}
 };
-
-Player.prototype.joinRoom = function(roomId) {
-	var cplayer = this;
-	console.log("cplayer: " + cplayer.ingame);
-	console.log("this: " + this.ingame);
-
-	if (this.manager.isJoinable(roomId)) {
-		this.loc(roomId); // Join room
-		this.manager.states[roomId].addPlayer(this); //Add player to gamestate's player list
-		this.inGame = true; //Set in player 'ingame' status to True
-
-		io.to(socket.id).emit('joinSuccess', roomId); //Tell client room join is successful.
-	}
-}
-
-
 module.exports = Player;
 
 
